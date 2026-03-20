04b94f9138e8e72ac6bfce242a272e4e7523e213
Mikołaj Błaszczyk <m.maba28@gmail.com>
2026-03-20 20:54:14 +0100

    wdrożenie własnego backendu Netlify (CRON + Blobs)

--- src/api/fetchNews.js
+++ src/api/fetchNews.js
@@ -1,13 +1,7 @@
-export const fetchNews = async () => {
-  const apiKey = import.meta.env.VITE_NEWS_API_KEY;
-
-  const url = `https://newsdata.io/api/1/latest?
-  apikey=${apiKey}
-  &language=en
-  &category=world,business,health,technology,science
-  &image=1
-  &removeduplicate=1`;
-  const response = await fetch(url);
+export const fetchNews = async (category = "all", page = 1) => {
+  const response = await fetch(
+    `/.netlify/functions/get-feed?category=${category}&page=${page}&limit=50`,
+  );
 
   if (!response.ok) {
     throw new Error(`Web error: ${response.status} - ${response.statusText}`);
